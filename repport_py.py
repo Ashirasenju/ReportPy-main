94a32145ab5636f632e28747f5d5da1a62c1ad5c
Ashirasenju2 <pa.soubrier@gmail.com>
2022-05-07 23:03:49 +0200

    Added basic OS information

--- repport_py.py
+++ repport_py.py
@@ -1,7 +1,23 @@
 import os
-import platform
+import platform as plat
+
 # get the basic information of the software
-report = open("report.txt")
+report = open("report.txt", "w")
+
+platform = plat.platform()
+system = plat.system()
+ver = plat.release()
+bits = plat.machine()
+computer_name = plat.node()
+
+report.write("Curplat: {} \n".format(platform))
+report.write("Cursys: {} \n".format(system))
+report.write("Curver: {} \n".format(ver))
+report.write('Nmbrbits: {} \n'.format(bits))
+report.write("CmpterName: {} \n \n".format(computer_name))
+
+# get the basic informations of the hardware
 
 
+report.write("HARDWARE \n")
 
