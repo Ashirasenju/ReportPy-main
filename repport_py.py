8c900f716de1f23bb3d94a839d5d9fa1f7142d14
Ashirasenju2 <pa.soubrier@gmail.com>
2022-05-07 23:26:55 +0200

    Added basic Hardware Information (not complete)

--- repport_py.py
+++ repport_py.py
@@ -1,6 +1,6 @@
 import os
 import platform as plat
-
+import psutil
 # get the basic information of the software
 report = open("report.txt", "w")
 
@@ -17,7 +17,18 @@ report.write('Nmbrbits: {} \n'.format(bits))
 report.write("CmpterName: {} \n \n".format(computer_name))
 
 # get the basic informations of the hardware
-
+cpu = plat.processor()
 
 report.write("HARDWARE \n")
+
+report.write("cores: {} \n".format(psutil.cpu_count(logical=False)))
+report.write("totcore: {} \n".format(psutil.cpu_count(logical=True)))
+# CPU frequencies
+cpufreq = psutil.cpu_freq()
+report.write(f"maxfreq: {cpufreq.max:.2f}Mhz \n")
+report.write(f"minfreq: {cpufreq.min:.2f}Mhz \n")
+ram_in_o = str(psutil.virtual_memory()[0])
+
+ram_in_MB = ram_in_o[0:2]
+report.write("ramtot: {}GB".format(ram_in_MB))
 
